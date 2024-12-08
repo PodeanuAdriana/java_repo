79a3d7900c48e7a5d2779906980f7dca63ac1a6a
Podeanu Adriana <stefaniapodeanu1@gmail.com>
2024-12-08 21:13:40 +0200

    read3_integers_from_stdin

---
+++ input_keyboard.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+//Challange Hackerrank-----> you must read 3 integers from stdin and then print them in stdout.
+//--------------->Each integer must be printed on new line
+public class input_keyboard {
+    public static void main(String args[]) {
+        // declare an scanner object that can read values from keyboard
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        // read a number from keyboard
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
