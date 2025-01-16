d4f021b165a8b28ab0277b2aec498b8b576075df
Podeanu Adriana <stefaniapodeanu1@gmail.com>
2025-01-16 17:20:16 +0200

    update_progression_java

--- progression_ar.java
+++ progression_ar.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class progression_ar {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = 0;
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int p = a;
+
+            for (int j = 0; j < n; j++) {
+                p += (int) Math.pow(2, j) * b;
+                System.out.print(p + " ");
+            }
+
+        }
+        in.close();
 
     }
 }
